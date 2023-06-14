16448f378a3d2cb9d2a47b39c0b53df3ff37eab2
Ihor Naidenko <Ihor_Naidenko@epam.com>
2023-06-14 17:19:04 +0300

    selenide-jenkins first commit

---
+++ src/test/java/tests/properties/SystemPropertyTests.java
@@ -0,0 +1,70 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertyTests {
+    @Test
+    void simplePropertyTest() {
+        String browserName = System.getProperty("browser");
+        System.out.println(browserName); // null
+    }
+
+    @Test
+    void simpleProperty1Test() {
+        System.setProperty("browser", "opera");
+        String browserName = System.getProperty("browser");
+        System.out.println(browserName); // opera
+    }
+
+    @Test
+    void simpleProperty2Test() {
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName); // firefox
+    }
+
+    @Test
+    void simpleProperty3Test() {
+        System.setProperty("browser", "opera");
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName); // opera
+    }
+
+    // под каждый @Tag создаем (так принято) отдельную таску в build.gradle,
+    // дает дополнительные возможности для конмбинации набора тестов
+    // но в простых случаях это не выглядит обязательным, достаточно при запуске из cmd указать тег напрямую
+
+    @Test
+    @Tag("one_property")
+    void simpleProperty4Test() {
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName);
+        // gradle clean one_property_test
+        // firefox
+
+        // gradle clean one_property_test -Dbrowser=safari
+        // safari
+    }
+
+    @Test
+    @Tag("hello")
+    void simpleProperty5Test() {
+        System.out.println("Hello, " + System.getProperty("user_name", "unknown student"));
+
+        /*
+        gradle clean hello_test
+            Hello, unknown student
+
+        gradle clean hello_test -Duser_name=Alex
+            Hello, Alex
+
+        gradle clean hello_test -Duser_name=Alex Egorov
+            FAILURE: Build failed with an exception.
+            Task 'Egorov' not found in root project 'qa-guru-jenkins-16'.
+
+        gradle clean hello_test "-Duser_name=Alex Egorov"
+        gradle clean hello_test -Duser_name="Alex Egorov"
+            Hello, Alex Egorov
+         */
+    }
+}
